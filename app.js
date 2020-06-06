22fcf5f6fe8ca20bba997a5ffab968e955b42eb9
Ez Ablaza <ezrabl42@gmail.com>
2020-06-06 03:16:26 -0500

    add models, set up mongoose

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const egql = require('express-graphql');
 const { buildSchema } = require('graphql');
+const mongoose = require('mongoose');
+
+const { List, Task } = require('./models/list');
 
 const app = express();
 
@@ -15,23 +18,46 @@ app.use(bodyParser.json());
 
 app.use('/api', egql({
   schema: buildSchema(`
-    
-    type List {
+
+    enum TaskState {
+      NOT_STARTED
+      IN_PROGRESS
+      ABANDONED
+      FINISHED
+    }
+
+    type Task {
       _id: ID!
       title: String!
       description: String
       author: String!
       creationDate: String!
-      size: Int!
+      taskState: TaskState!
     }
 
+    input TaskInput {
+      title: String!
+      description: String
+      author: String!
+      taskState: TaskState!
+      listID: ID!
+    }
+
+
+    type List {
+      _id: ID!
+      title: String!
+      description: String
+      author: String!
+      creationDate: String
+      tasks: [Task!]
+      percentDone: Float
+    }
 
     input ListInput {
       title: String!
       description: String
       author: String!
-      creationDate: String!
-      size: Int!
     }
 
     type RootQuery {
@@ -40,6 +66,7 @@ app.use('/api', egql({
 
     type RootMutation {
       createList(listInput: ListInput): List
+      createTask(taskInput: TaskInput): Task
     }
 
     schema {
@@ -49,30 +76,73 @@ app.use('/api', egql({
   `),
   rootValue: { 
     lists:() => {
-      return tempLists;
+      return List.find()
+        .then(lists => {
+          return lists.map(list => {
+            return { ...list._doc };
+          });
+        })
+        .catch(err => {
+          console.log(err);
+          throw err;
+        });
     },
     createList: (args) => {
 
-
-      
       const listArgs = args.listInput;
-      const newList = {
-        _id: Math.random().toString(),
+      const newList = new List({
+      
         title:  listArgs.title,
         description:  listArgs.description,
         author: listArgs.author,
-        creationDate: listArgs.creationDate,
-        size: +listArgs.size
+        creationDate: new Date(),
+        tasks: [],
+        percentDone: null
+      });
+
+      return newList.save()
+        .then(result => {
+          return { ...result._doc };
+        })
+        .catch(err => {
+          console.log(err);
+          throw err;
+      });
+    
+    },
+    createTask: (args) => {
+
+      const taskArgs = args.taskInput;
+      const owningList = tempLists.find(list => list._id === taskArgs.listID);
+      
+      const newTask = {
+        _id: Math.random().toString(),
+        title: taskArgs.title,
+        description: taskArgs.description,
+        author: taskArgs.author,
+        creationDate: taskArgs.creationDate,
+        taskState: taskArgs.taskState,
       };
+      
+      owningList.tasks.push(newTask);
 
-      tempLists.push(newList);
-
-      return newList;
+      return newTask;
     }
   },  
   graphiql: true
 }));
 
 
-app.listen(4000);
+mongoose.connect(
+  `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@otp-cluster-syomc.gcp.mongodb.net/` +
+  `${process.env.MONGO_DB}?authSource=admin&retryWrites=true&w=majority`
+  ).then(() => {
+    app.listen(4000);
+  }).catch((err) => {
+    console.log(err);
+  });
+
+
+
+
 
---
+++ models/list.js
@@ -0,0 +1,83 @@
+//list.js
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+
+const TaskSchema = new Schema({
+
+  title: {
+
+    type: String,
+    required: true
+  },
+  description: {
+
+    type: String,
+    required: false
+  },
+  author: {
+    
+    type: String,
+    required: true
+  },
+  creationDate: {
+    
+    type: Date,
+    required: true
+  },
+  taskState: {
+
+    type: String,
+    required: true,
+    enum: [
+      'NOT_STARTED',
+      'IN_PROGRESS',
+      'ABANDONED',
+      'FINISHED'
+    ]
+  }
+});
+
+const ListSchema = new Schema({
+
+  title: {  
+    
+    type: String,
+    required: true
+  },
+  description: {
+    
+    type: String,
+    required: false
+  },
+  author: {
+
+    type: String,
+    required: true
+  },
+  creationDate: {
+
+    type: Date,
+    required: true
+  },
+  tasks: {
+
+    type: [TaskSchema],
+    required: true
+  },
+  percentDone: {
+
+    type: Number,
+    required: false
+  }
+
+});
+
+
+
+module.exports = {
+  
+  List: mongoose.model('List', ListSchema),
+  Task: mongoose.model('Task', TaskSchema)
+};
