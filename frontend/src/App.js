7c0f54323fd5c91162813ed8846afd8c6c1fa7be
Ez Ablaza <ezrabl42@gmail.com>
2020-06-20 19:45:40 -0500

    move away from CRA for now, but adding appropriate PATH for WSL fixes many issues

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
-import { BrowserRouter, Route } from 'react-router-dom'
+import { BrowserRouter, Route, Redirect } from 'react-router-dom'
 
+import AuthPage from './components/Auth';
+import Blank from '.';
 
 class App extends Component {
-  
+
   render() {
     return (
-      <BrowserRouter>
-        <Route path="/" component={null} />
-        <Route path="/auth" component={null} />
+			<BrowserRouter>
+				<Redirect path="/" to="/auth" />
+        <Route path="/auth" component={AuthPage} />
         <Route path="/lists" component={null} />
         <Route path="/search" component={null} />
       </BrowserRouter>
--- frontend/src/components/Auth.js
+++ frontend/src/components/Auth.js
@@ -1,1 +1,13 @@
+import React, { Component } from 'react';
 
+class AuthPage extends Component {
+
+	render() {
+
+		return (
+			<h1>Auth page</h1>
+		);
+	}
+}
+
+export default AuthPage;
