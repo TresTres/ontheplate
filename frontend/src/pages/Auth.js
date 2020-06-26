4d406186fffd404e77d9fc3522bd1aeac44c22eb
Ez Ablaza <ezrabl42@gmail.com>
2020-06-26 01:21:34 -0500

    enable user creation via auth page

--- frontend/src/pages/Auth.js
+++ frontend/src/pages/Auth.js
@@ -8,24 +8,38 @@ import {
 	InputLabel
 } from '@material-ui/core';
 
+import { validateInput } from '../utils/helper';
 import './Auth.css';
 
 class AuthPage extends Component {
 
+
+	state = {
+		isLogin: true
+	};
+
 	constructor(props) {
 		super(props);
 		this.emailEl = React.createRef();
+		this.userNameEl = React.createRef();
 		this.passwordEl = React.createRef();
+	}
+
+	switchModeHandler = () => {
+		this.setState(prevState => {
+
+			return {isLogin: !prevState.isLogin};
+		});
 	}
 
 
 	loginHandler = (event) => {
 		event.preventDefault();
 	
-		const email = this.emailEl.value;
-		const password = this.passwordEl.value;
+		const email = this.emailEl.value,
+					password = this.passwordEl.value;
 
-		if(email.trim().length === 0 || password.trim() === 0) {
+		if (validateInput([email, password]) === false) {
 			return;
 		}
 
@@ -37,47 +51,103 @@ class AuthPage extends Component {
 					}
 				}
 			`
-		}
+		};
 
 		fetch('http://localhost:4000/api', {
 			method: 'POST',
 			body: JSON.stringify(requestBody),
 			headers: {
-				'Content-Type': 'application/json',
+				'Content-Type': 'application/json'
 			}
-		})
-		.then(res => {
-			if(res.status !== 200 && res.status !== 201) {
+		}).
+		then((res) => {
+			if (res.status !== 200 && res.status !== 201) {
 				throw new Error('Failed');
 			}
 			return res.json();
-		})
-		.then(resbody => {
+		}).
+		then((resbody) => {
 			console.log(resbody);
-		})
-		.catch(err => {
+		}).
+		catch((err) => {
 			console.log(err);
-		})
+		});
 	};
+
+	createUserHandler = (event) => {
+
+		event.preventDefault();
+		const email = this.emailEl.value, 
+					password = this.passwordEl.value,
+					userName = this.userNameEl.value;
+
+		if (validateInput([email, password, userName]) === false) {
+			return;
+		}
+
+		const requestBody = {
+			query: `
+				mutation {
+					createUser(userInput: {
+						email: "${email}",
+						password: "${password}",
+						userName: "${userName}"
+					}) {
+						userName,
+						email,
+						creationDate
+					}
+				}
+			`
+		};
+		
+		fetch('http://localhost:4000/api', {
+			method: 'POST',
+			body: JSON.stringify(requestBody),
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		}).
+		then((res) => {
+			if (res.status !== 200 && res.status !== 201) {
+				throw new Error('Failed');
+			}
+			return res.json();
+		}).
+		then((resbody) => {
+			console.log(resbody);
+		}).
+		catch((err) => {
+			console.log(err);
+		});
+	};
+
 
 	render() {
 
 		return (
-			<form className="auth-form" onSubmit={this.loginHandler}>
+			<form className="auth-form" 
+				onSubmit={this.state.isLogin ? this.loginHandler : this.createUserHandler}>
 				<FormControl required fullWidth={true} className="form-control">
 					<InputLabel htmlFor="email">E-Mail</InputLabel>
-					<Input type="email" id="email-input" inputRef={ref => {this.emailEl = ref}}/>
+					<Input type="email" id="email-input" inputRef={(ref) => {this.emailEl = ref}}/>
 				</FormControl>
+				{ !this.state.isLogin &&
+					<FormControl required fullWidth={true} className="form-control">
+						<InputLabel htmlFor="userName">User Name</InputLabel>
+						<Input type="username" id="username-input" inputRef={(ref) => {this.userNameEl = ref}}/>
+					</FormControl> }
 				<FormControl required fullWidth={true} className="form-control">
 					<InputLabel htmlFor="password">Password</InputLabel>
-					<Input type="password" id="password" inputRef={ref => {this.passwordEl = ref}}/>
+					<Input type="password" id="password-input" inputRef={(ref) => {this.passwordEl = ref}}/>
 				</FormControl>
 				<div className="form-actions">
 					<Button variant="outlined" type="submit" disableElevation>
-						Log In
+						{this.state.isLogin ? 'Log In' : 'Create Account'}
 					</Button>
-					<Button variant="outlined" type="button" disableElevation>
-						Create User
+					<Button variant="outlined" type="button" onClick={this.switchModeHandler} 
+						disableElevation>
+						{this.state.isLogin ? 'New User' : 'Back to Sign In'}
 					</Button>
 				</div>
 			</form>
